858d61fa74aa7df70477c14ae6ab4f73e3623a0a
MARCEL GROSZKOWSKI <s30689@pjwstk.edu.pl>
2024-04-17 11:05:50 +0200

    17.04

---
+++ untitled/src/Student.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    public String fname;
+    public String lname;
+    public int indexNumber;
+    public String email;
+    public String address;
+    public int grade;
+    public String przedmiot;
+
+
+
+
+    static ArrayList<Student> students;
+
+    public static void enrollStudent(String przedmiot){
+
+    }
+
+
+    public static void addGrade(int grade, String przedmiot){
+
+    }
+
+
+}
---
+++ untitled/src/StudyProgramme.java
@@ -0,0 +1,12 @@
+public class StudyProgramme {
+    String kiernekNazwa;
+    String kierunekOpis;
+    int semestryAmount;
+    int intAmount;
+
+
+
+    public StudyProgramme(String kiernekNazwa, String kierunekOpis, int semestry,int semestryAmount){
+
+    }
+}
